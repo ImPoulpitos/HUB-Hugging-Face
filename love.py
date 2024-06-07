fa1840293f7281e7ae74473ecd3a950ec01064bf
Poulpitos <58633532+ImPoulpitos@users.noreply.github.com>
2024-06-07 11:46:40 +0200

    feat: add more examples

---
+++ love.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="SchuylerH/bert-multilingual-go-emtions")
+
+texte = "Antoine, my lover."
+resultats = pipe(texte)
+
+for resultat in resultats:
+    print(f"Label: {resultat['label']}, Score: {resultat['score']:.4f}")
